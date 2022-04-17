e22d4793dd9f6f6300809f03008fbf07a54c92e1
scx567888 <scx567888@outlook.com>
2022-04-18 00:00:13 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import {createApp} from 'vue';
 import App from './App.vue';
-import {ComponentInstaller} from "../_node_modules/ttttt/index.js";
+
+
+// spark-md5 can be correctly import in utils/index.js
+import {ComponentInstaller} from "../_node_modules/my-lib/index.js";
+
+
+// Uncaught SyntaxError: The requested module '/node_modules/spark-md5/spark-md5.js?v=0aa2830a' does not provide an export named 'default' (at index.js:1:8)
+// import {ComponentInstaller} from "my-lib";
 
 createApp(App)
     .use(ComponentInstaller)
